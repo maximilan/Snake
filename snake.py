ddf30327b5343bef4d61c3a81c06e3b0e09601fd
maximilian <39700327+maximilan@users.noreply.github.com>
2018-07-26 12:20:14 +0200

    Game Over screen added

--- snake.py
+++ snake.py
@@ -1,8 +1,11 @@
-Width = 200
-snakethick = 4
+Width = 400
+snakethick = 8
 rows = Width / snakethick
 current_key = "Right"
 game = True
+running = True
+start = True
+current_direction = None
 
 
 class Point():
@@ -11,16 +14,16 @@ class Point():
         self.y = y
         self.fill = fill
         self.counter = 1
+
     def draw(self):
-        '''if self.counter < 6:
-            self.counter += 1
-            c.create_rectangle(self.x, self.y, self.x + snakethick, self.y + snakethick, fill = 'black', outline = 'black')
-        else:'''
         c.create_rectangle(self.x, self.y, self.x + snakethick, self.y + snakethick, fill = self.fill, outline = self.fill)
+
     def add(self, x, y):
         return Point(self.x + x, self.y + y)
+
     def return_coords(self):
         return self.x, self.y
+
     def equals(self, p):
         x, y = p.return_coords()
         if x == self.x and y == self.y:
@@ -51,6 +54,7 @@ class Snake():
         global current_key
         global game
         global Width
+        global current_direction
         change = (self.counter % self.length)
         print(change)
         new = self.head
@@ -63,6 +67,7 @@ class Snake():
             self.body[change] = new.add(snakethick, 0)
         elif current_key == "Left":
             self.body[change] = new.add(-snakethick, 0)
+        current_direction = current_key
         self.prevhead = self.head
         self.head = self.body[change]
         self.counter += 1
@@ -95,15 +100,24 @@ class Snake():
         while (self.counter % self.length) != old:
             self.counter += 1
         self.body.append(None)
-        
+
     def return_body(self):
         return self.body
 
 from tkinter import *
+import tkinter as tk
 from time import sleep
 from random import randint
+def close_window():
+    global running
+    running = False
+    global game
+    game = False
+    root.destroy()
 
 root = Tk()
+root.title = "Snake"
+root.protocol("WM_DELETE_WINDOW", close_window)
 c = Canvas(width = Width, height = Width, bg = 'white')
 c.pack()
 snake = Snake()
@@ -111,18 +125,57 @@ item = Item()
 
 def movement(event):
     global current_key
-    current_key = str(event.keysym)
+    if str(event.keysym) in ("Right", "Left", "Up", "Down"):
+        if current_direction == "Right" and str(event.keysym) == "Left" or current_direction == "Left" and str(event.keysym) == "Right":
+            None
+        elif current_direction == "Up" and str(event.keysym) == "Down" or current_direction == "Down" and str(event.keysym) == "Up":
+            None
+        else:
+            current_key = str(event.keysym)
 c.bind_all('<Key>', movement)
 
 def show():
     global Width
     c.delete("all")
     c.create_rectangle(0, 0, Width + 100, Width + 100, fill = 'black', outline = 'black')
-    #c.create_rectangle(snakethick, snakethick, Width - snakethick, Width - snakethick, fill = 'white', outline = 'white')
     snake.update()
     item.show()
     root.update()
     sleep(0.1)
 
-while game:
-    show()
+def start_game():
+    global start
+    start = False
+
+def init():
+    global start
+    start = True
+    c.create_rectangle(0, 0, Width + 100, Width + 100, fill = 'black', outline = 'black')
+    c.create_text(Width/2, Width/2 - 50, text = "Snake", font=('Lato Black', 50), fill = 'white')
+    c.create_text(Width/2, Width/2 + 20, text = "A production of HERFARTH Enterprises", font=('Lato Black', 10), fill = 'white')
+    B = tk.Button(root, text="Neues Spiel", command = start_game)
+    B.pack(side = LEFT)
+    while start:
+        if running = False:
+            break
+        root.update()
+    B.destroy()
+    start = True
+
+def gameover():
+    c.create_text(Width/2, Width/2 -50, text = "Game over", font=('Lato Black', 60), fill = 'white')
+    c.create_text(Width/2, Width/2+30, text = "Points: " + str(len(snake.return_body())), font=('Lato Black', 30), fill = 'white')
+    root.update()
+    sleep(5)
+    if running = False:
+        break
+    c.delete("all")
+
+
+while running:
+    init()
+
+    while game:
+        show()
+
+    gameover()
